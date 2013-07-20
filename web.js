feb426b1dadc9849eee67cad56b2533146010916
ilPaschi <stefano.galli@gmail.com>
2013-07-21 01:33:48 +0200

    Response body from external HTML file

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var buf = require('buffer');
+
 var app = express();
 app.use(express.logger());
 
+var readstring = fs.readFileSync('index.html');
+var buf = new Buffer(readstring);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
